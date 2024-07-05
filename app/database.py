1fc593ecc655531bcab081fd1c58629d03e2f9b6
yugo19 <doucouremohamed8@gmail.com>
2024-07-05 17:12:48 +0000

    new updates

--- app/database.py
+++
@@ -1,5 +0,0 @@
-from databases import Database
-
-postgres_url = "postgresql://root:postges@139.144.63.238/mapaction"
-# postgres_url = "postgresql://postgres:postges@localhost/mapaction"
-database = Database(postgres_url)
